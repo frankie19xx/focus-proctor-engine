e5ce86f088d22bce57aed03222483a196926105e
frankie19xx <emmanuelfrank655@gmail.com>
2026-07-04 18:17:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { SignedIn, SignedOut, RedirectToSignIn, useUser } from "@clerk/clerk-react";
 
-// Import your pages
 import Login from "./pages/Login";
 import StudentDashboard from "./pages/StudentDashboard";
 import LecturerDashboard from "./pages/LecturerDashboard";
@@ -10,13 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Root URL automatically redirects to Login */}
         <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* Login Page */}
         <Route path="/login" element={<Login />} />
 
-        {/* Student Dashboard */}
         <Route
           path="/dashboard"
           element={
@@ -26,7 +21,6 @@ function App() {
           }
         />
 
-        {/* Lecturer Dashboard */}
         <Route
           path="/lecturer"
           element={
@@ -36,7 +30,6 @@ function App() {
           }
         />
 
-        {/* Catch all unauthorized users */}
         <Route path="*" element={<RedirectToSignIn />} />
       </Routes>
     </Router>
