e9ade135405f35a457ed38492941ae8b986e9dc3
frankie19xx <emmanuelfrank655@gmail.com>
2026-07-04 18:44:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { SignedIn, SignedOut, RedirectToSignIn, useUser } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
+// Import your pages
 import Login from "./pages/Login";
 import StudentDashboard from "./pages/StudentDashboard";
 import LecturerDashboard from "./pages/LecturerDashboard";
@@ -9,27 +10,55 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* Root URL: send signed-in users to their dashboard, everyone else to login */}
+        <Route
+          path="/"
+          element={
+            <>
+              <SignedIn>
+                <Navigate to="/dashboard" replace />
+              </SignedIn>
+              <SignedOut>
+                <Navigate to="/login" replace />
+              </SignedOut>
+            </>
+          }
+        />
+
+        {/* Login Page */}
         <Route path="/login" element={<Login />} />
 
+        {/* Student Dashboard */}
         <Route
           path="/dashboard"
           element={
-            <SignedIn>
-              <StudentDashboard />
-            </SignedIn>
+            <>
+              <SignedIn>
+                <StudentDashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
 
+        {/* Lecturer Dashboard */}
         <Route
           path="/lecturer"
           element={
-            <SignedIn>
-              <LecturerDashboard />
-            </SignedIn>
+            <>
+              <SignedIn>
+                <LecturerDashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
 
+        {/* Catch all unauthorized users */}
         <Route path="*" element={<RedirectToSignIn />} />
       </Routes>
     </Router>
